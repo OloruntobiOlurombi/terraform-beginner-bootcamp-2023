dd49f0c1905c306496ebaddc6da87cd419b85f14
Oloruntobi Olurombi <40290711+OloruntobiOlurombi@users.noreply.github.com>
2023-11-06 21:11:54 +0100

    21 restructure root module (#22)
    * Update week1.md
    
    * #21 change structure of root module and add tagging to bucket

--- main.tf
+++ main.tf
@@ -1,37 +1,3 @@
-terraform {
-  cloud {
-    organization = "terraform-begineer-bootcamp-2023"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-
-
-provider "aws" {
-  # Configuration options
-  #region = "us-east-1"
-  #access_key = 
-  #secret_key = 
-}
-
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs
-
-provider "random" {
-  # Configuration options
-}
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 
@@ -46,8 +12,8 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-    value = random_string.bucket_name.result 
+    tags = {
+    UserUuid        = var.user_uuid 
+  }
 }
