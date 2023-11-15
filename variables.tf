f69367b4320345be2d7b865e329f16cf60269487
Oloruntobi Olurombi <40290711+OloruntobiOlurombi@users.noreply.github.com>
2023-11-15 16:34:29 +0100

    #23 bucket should no longer use random provider (#24)

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "user_uuid" {
     error_message = "The 'user_uuid' must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)."
   }
 }
+
+variable "buket_name" {
+  type = string 
+  default = "f17a0oifpuowri4pnl58r53bjwkkfqbq"
+}
